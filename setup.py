fd75355a0e4cce4b2468a5a3b34d9b7827362326
Genevieve Okon <okong@mcmaster.ca>
2024-07-25 18:59:30 -0400

    version 2 changes

--- setup.py
+++ setup.py
@@ -1,29 +1,8 @@
-# setup.py
-from setuptools import setup
-
-setup(
-    name='upperTolower',
-    version='0.1',
-    py_modules=['texttransform'],
-    entry_points={
-        'console_scripts': [
-            'upperTolower = text_transform:main',
-        ],
-    },
-    description='A simple tool to convert uppercase sentences to lowercase and vice versa and also create correct cases for sentences.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Genevevieve',
-    author_email='',
-    url='https://github.com/Genevieveok/texttransform',
-)
-
-
 from setuptools import setup
 
 setup(
     name='texttransform',
-    version='0.1',
+    version='0.2',
     py_modules=['text_transform'],
     entry_points={
         'console_scripts': [
